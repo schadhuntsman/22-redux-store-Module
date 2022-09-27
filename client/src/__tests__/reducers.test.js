91fc281c6ca8a8cb58d8ab01aa5f5335261c43f2
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-09-27 16:02:10 -0700

    updated with tests

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,16 @@
+//import actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions'
+
+//create a sample of what our global state will look like
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+};
+
+expect(newState.produce.).toBe(2);
+expect(initialState).products
