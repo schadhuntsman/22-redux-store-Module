5a7302396d17c7621ca53f5e0ade396dde11d854
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-09-28 00:15:08 -0700

    updated tests and resolvers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,5 @@
+import { useReducer } from 'react';
+
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
@@ -6,14 +8,29 @@ import {
 
 export const reducer = (state, action) => {
     switch (action.type) {
-        //if action value is the value of "update products"return a new state object with an updated products array 
-        case UPDATE_PRODUCTS: 
+      // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+      case UPDATE_PRODUCTS:
         return {
-            ...state,
-            products: [...action.products],
+          ...state,
+          products: [...action.products]
         };
-        //if it's none of these actions, do notupdate state at all and keep everything the same
-        defualt:
+      // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories]
+        };
+  
+      default:
         return state;
     }
-};
+    case UPDATE_CATEGORY:
+        return {
+            ...state,
+            currentCategory: action.currentCategory
+        }
+
+        export function useProductReducer(initialState) {
+            return useReducer(reducer, initialState);
+        }
+  };
